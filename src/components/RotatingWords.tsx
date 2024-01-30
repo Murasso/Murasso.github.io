08016a280604e82ff127cb724dd4078a6a02a5d3
Murasso <sou04136@gmail.com>
2024-01-30 21:41:36 +0900

    finish

--- src/components/RotatingWords.tsx
+++ src/components/RotatingWords.tsx
@@ -1,28 +1,58 @@
 // RotatingWords.jsx
 
-import React, { useRef, useEffect } from 'react';
-import { init } from 'ityped';
+import React, { useState, useEffect } from 'react';
+import './RotatingWords.css';
 
 const RotatingWords = () => {
-  const textRef = useRef();
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Web Developer", "AI Engineer", "UI/UX Designer"];
+  const period = 2000;
 
   useEffect(() => {
-    const options = {
-      strings: ["Web Developer", "Web Designer", "UI/UX Designer"],
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 3000, // Pause for 3 seconds before backtyping
-      loop: true,
-    };
+    const ticker = setInterval(tick, delta);
 
-    init(textRef.current, options);
-  }, []);
+    return () => clearInterval(ticker);
+  }, [text, delta]);
+
+  const tick = () => {
+    const i = loopNum % toRotate.length;
+    const fullText = toRotate[i];
+    const updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(100); // Faster backspacing
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(500);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum((prevLoopNum) => prevLoopNum + 1);
+      setIndex(1);
+      setDelta(200); // Faster typing
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
 
   return (
-    <div className="rotating-words">
+    <span className="rounded-sm z-[100] font-semibold rotating-words">
       {`Hi! I'm So `}
-      <span ref={textRef}></span>
-    </div>
+      <span className="rounded-sm font-semibold txt-rotate white-text" data-period="1000" data-rotate={JSON.stringify(toRotate)}>
+        <span className="wrap">{text}</span>
+        <span className="cursor-line"></span>
+      </span>
+    </span>
   );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES  // この行を追加
-        ? "Murasso.github.io"            // この行を追加
-        : "./",
+  base: process.env.GITHUB_PAGES ? "Murasso.github.io" : "./",
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['react-type-animation'],
+  },
+});
