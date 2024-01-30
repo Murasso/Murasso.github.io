fa1711d40d770a3a19e9b21a548584ca0190d420
Murasso <sou04136@gmail.com>
2024-01-31 08:59:54 +0900

    finish

--- src/scenes/MySkills.tsx
+++ src/scenes/MySkills.tsx
@@ -31,6 +31,100 @@ const MySkills = () => {
 
                 </p>
                 </motion.div>
+                <div className="mt-16 md:mt-0">
+                    {isAboveMediumScreens ? (
+                    <div
+                        className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+                        before:w-full md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+                      >
+                        <img
+                          alt="skills"
+                          className="z-10"
+                          src="/skills-image.png"
+                        />
+                        </div>
+                    ):(
+                        <img
+                          alt="skills"
+                          className="z-10"
+                          src="/skills-image.png"
+                        />
+                    )}
+
+                </div>
+
+            </div>
+
+            <div className="md:flex md:justify-between mt-16 gap-32">
+            <motion.div
+                className="md:w-1/3 mt-10"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
+                variants={{
+                    hidden: { opacity: 0, y: 50 },
+                    visible: { opacity: 1, y: 0 },
+                }}
+             >
+                <div className="relative h-32">
+                    <div className="z-10">
+                        <p className="font-playfair font font-semibold text-5xl">01</p>
+                        <p className="font-playfair font font-semibold text-3xl mt-3">Experience</p>
+                    </div>
+                    <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]"/>
+                </div>
+                <p className="mt-5">
+                    Henlo
+                </p>
+
+                </motion.div>
+            <motion.div
+                className="md:w-1/3 mt-10"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.4, duration: 0.5 }}
+                variants={{
+                    hidden: { opacity: 0, y: 50 },
+                    visible: { opacity: 1, y: 0 },
+                }}
+             >
+                <div className="relative h-32">
+                    <div className="z-10">
+                        <p className="font-playfair font font-semibold text-5xl">02</p>
+                        <p className="font-playfair font font-semibold text-3xl mt-3">Innovative</p>
+                    </div>
+                    <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]"/>
+                </div>
+                <p className="mt-5">
+                    Henlo
+                </p>
+
+                </motion.div>
+            <motion.div
+                className="md:w-1/3 mt-10"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.6, duration: 0.5 }}
+                variants={{
+                    hidden: { opacity: 0, y: 50 },
+                    visible: { opacity: 1, y: 0 },
+                }}
+             >
+                <div className="relative h-32">
+                    <div className="z-10">
+                        <p className="font-playfair font font-semibold text-5xl">03</p>
+                        <p className="font-playfair font font-semibold text-3xl mt-3">Experience</p>
+                    </div>
+                    <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]"/>
+                </div>
+                <p className="mt-5">
+                    Henlo
+                </p>
+
+                </motion.div>
             </div>
         </section>
     )
